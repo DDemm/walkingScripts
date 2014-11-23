a7f407fbd597421576a18b235b83703806f60589
Dan <dan.d.emmerson@gmail.com>
2014-11-23 17:44:22 +0000

    Character mover scripts into folder

---
+++ characterMover.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class characterMover : MonoBehaviour {
+
+	public float speed=1f;
+	//Checking GIT
+	void Update() 
+	{
+		transform.Translate(Vector3.forward * speed * Time.deltaTime*speed);
+	}
+}
